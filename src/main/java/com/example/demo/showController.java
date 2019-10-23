a3bb14df056a9b7b3f36275aff5e8b6ec6c13fe1
dake3909 <13543306092@163.com>
2019-10-23 21:34:02 +0800

    新增poi操作excel的demo

--- src/main/java/com/example/demo/showController.java
+++ src/main/java/com/example/demo/showController.java
@@ -1,12 +1,21 @@
 package com.example.demo;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class showController {
@@ -16,5 +25,135 @@ public class showController {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("test");
         return mv;
+    }
+
+    public void generateExcel() throws Exception {
+        Workbook wb = new HSSFWorkbook();
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream("C:\\Users\\13543\\Desktop\\text.xlsx");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        wb.createSheet("sheet1");
+        wb.write(fos);
+    }
+
+    public static boolean createWorkBook(String excelType, String sheetName, List<String> headList,List<List<String>> dataList,String path){
+        Workbook wb = null;
+        /*创建文件*/
+        if (excelType == null || excelType.endsWith("2003")) {
+            /*操作Excel2003以前（包括2003）的版本，扩展名是.xls */
+            wb = new HSSFWorkbook();
+        }else if (excelType.endsWith("2007")){
+            /*XSSFWorkbook:是操作Excel2007的版本，扩展名是.xlsx */
+            wb = new XSSFWorkbook();
+        }else {   //默认为2003版本
+            /*操作Excel2003以前（包括2003）的版本，扩展名是.xls */
+            wb = new HSSFWorkbook();
+        }
+        /*Excel文件创建完毕*/
+        CreationHelper createHelper = wb.getCreationHelper();  //创建帮助工具
+
+        /*创建表单*/
+        Sheet sheet = wb.createSheet(sheetName!=null?sheetName:"new sheet");
+        // Note that sheet name is Excel must not exceed 31 characters（注意sheet的名字的长度不能超过31个字符，若是超过的话，会自动截取前31个字符）
+        // and must not contain any of the any of the following characters:（不能包含下列字符）
+        // 0x0000  0x0003  colon (:)  backslash (\)  asterisk (*)  question mark (?)  forward slash (/)  opening square bracket ([)  closing square bracket (])
+        /*若是包含的话，会报错。但有一个解决此问题的方法，
+        就是调用WorkbookUtil的createSafeSheetName(String nameProposal)方法来创建sheet name,
+        若是有如上特殊字符，它会自动用空字符来替换掉，自动过滤。*/
+        /*String safeName = WorkbookUtil.createSafeSheetName("[O'Brien's sales*?]"); // returns " O'Brien's sales   "过滤掉上面出现的不合法字符
+        Sheet sheet3 = workbook.createSheet(safeName); //然后就创建成功了*/
+        /*表单创建完毕*/
+
+        //设置字体
+        Font headFont = wb.createFont();
+        headFont.setFontHeightInPoints((short)14);
+        headFont.setFontName("Courier New");
+        headFont.setItalic(false);
+        headFont.setStrikeout(false);
+        //设置头部单元格样式
+        CellStyle headStyle = wb.createCellStyle();
+        headStyle.setBorderBottom(BorderStyle.THICK);  //设置单元格线条
+        headStyle.setBottomBorderColor(IndexedColors.BLACK.getIndex());   //设置单元格颜色
+        headStyle.setBorderLeft(BorderStyle.THICK);
+        headStyle.setLeftBorderColor(IndexedColors.BLACK.getIndex());
+        headStyle.setBorderRight(BorderStyle.THICK);
+        headStyle.setRightBorderColor(IndexedColors.BLACK.getIndex());
+        headStyle.setBorderTop(BorderStyle.THICK);
+        headStyle.setTopBorderColor(IndexedColors.BLACK.getIndex());
+        headStyle.setAlignment(HorizontalAlignment.CENTER);    //设置水平对齐方式
+        headStyle.setVerticalAlignment(VerticalAlignment.CENTER);  //设置垂直对齐方式
+        //headStyle.setShrinkToFit(true);  //自动伸缩
+        headStyle.setFont(headFont);  //设置字体
+        /*设置数据单元格格式*/
+        CellStyle dataStyle = wb.createCellStyle();
+        dataStyle.setBorderBottom(BorderStyle.THIN);  //设置单元格线条
+        dataStyle.setBottomBorderColor(IndexedColors.BLACK.getIndex());   //设置单元格颜色
+        dataStyle.setBorderLeft(BorderStyle.THIN);
+        dataStyle.setLeftBorderColor(IndexedColors.BLACK.getIndex());
+        dataStyle.setBorderRight(BorderStyle.THIN);
+        dataStyle.setRightBorderColor(IndexedColors.BLACK.getIndex());
+        dataStyle.setBorderTop(BorderStyle.THIN);
+        dataStyle.setTopBorderColor(IndexedColors.BLACK.getIndex());
+        dataStyle.setAlignment(HorizontalAlignment.LEFT);    //设置水平对齐方式
+        dataStyle.setVerticalAlignment(VerticalAlignment.CENTER);  //设置垂直对齐方式
+        //dataStyle.setShrinkToFit(true);  //自动伸缩
+        /*创建行Rows及单元格Cells*/
+        Row headRow = sheet.createRow(0); //第一行为头
+        for (int i=0;i<headList.size();i++){  //遍历表头数据
+            Cell cell = headRow.createCell(i);  //创建单元格
+            cell.setCellValue(createHelper.createRichTextString(headList.get(i)));  //设置值
+            cell.setCellStyle(headStyle);  //设置样式
+        }
+
+        int rowIndex = 1;  //当前行索引
+        //创建Rows
+        for (List<String> rowdata : dataList){ //遍历所有数据
+            Row row = sheet.createRow(rowIndex++); //第一行为头
+            for (int j = 0;j< rowdata.size();j++){  //编译每一行
+                Cell cell = row.createCell(j);
+                cell.setCellStyle(dataStyle);
+                cell.setCellValue(createHelper.createRichTextString(rowdata.get(j)));
+            }
+        }
+        /*创建rows和cells完毕*/
+
+        /*设置列自动对齐*/
+        for (int i =0;i<headList.size();i++){
+            sheet.autoSizeColumn(i);
+        }
+        Row row = sheet.createRow(2);
+        Cell cell = row.createCell(1);
+        cell.setCellValue("33");
+        // 分别为起始行，结束行，起始列，结束列
+        CellRangeAddress region =  new CellRangeAddress(1,2,0,0);
+        sheet.addMergedRegion(region);
+        try  (OutputStream fileOut = new FileOutputStream(path)) {    //获取文件流
+            wb.write(fileOut);   //将workbook写入文件流
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        showController s = new showController();
+       // s.generateExcel();
+        List<String> headList = new ArrayList<>();
+        headList.add("name");
+        headList.add("age");
+        List<List<String>> dataList = new ArrayList<>();
+        List<String> rowOne = new ArrayList<>();
+        rowOne.add("susan");
+        rowOne.add("22");
+        dataList.add(rowOne);
+        createWorkBook("2007","sheet1",headList,dataList,"C:\\Users\\13543\\Desktop\\11.xlsx");
     }
 }
